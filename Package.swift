793cbe3dcf566109e3d2df2226f7f3064a62df7e
Vitalii <870237+ikorich@users.noreply.github.com>
2026-09-15 13:50:20 +0300

    Update SimplePing dependency version to 0.1.2

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/IGRSoft/SimplePing.git",
-            from: "0.1.1")
+            from: "0.1.2")
     ],
     targets: [
         .target(
